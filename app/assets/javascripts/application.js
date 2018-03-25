ace73ebec25091dc40b1db0d9c59740815fa7223
Oleksandr Ostach <ukrorion@gmail.com>
2018-03-26 02:08:49 +0300

    Add simple navigation and fontawesome lib

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,9 @@
 import '../stylesheets/application.sass'
 
 import 'bootstrap/dist/js/bootstrap.min'
+
+import fontawesome from '@fortawesome/fontawesome'
+import solid from '@fortawesome/fontawesome-free-solid'
+import regular from '@fortawesome/fontawesome-free-regular'
+fontawesome.library.add(solid)
+fontawesome.library.add(regular)
