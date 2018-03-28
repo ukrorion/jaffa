b1ab5bee6050bef0546250553ec10b671e3c43ea
Oleksandr Ostach <ukrorion@gmail.com>
2018-03-29 00:15:03 +0300

    Add user validations and fix registration form

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable, :confirmable, :lockable
 
   has_one :address, as: :addressable
+
+  validates :tax_number, presence: true, format: { with: /\A\d+\z/, message: "only allows letters" }, length: { is: 10 }
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :middle_name, presence: true
 end
